65df2e76ae4c128f51c44554ab9f1386170de575
kuanov007 <kuanovt1@gmail.com>
2026-04-24 18:45:42 +0500

    Queries added

--- src/main/java/com/toir/repository/UserRepository.java
+++ src/main/java/com/toir/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.toir.repository;
 import org.springframework.stereotype.Repository;
 import com.toir.entity.User;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,11 +14,9 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
-    @EntityGraph(attributePaths = {"primaryRole", "roles"})
     @Query(value = "SELECT * FROM users WHERE username = :username LIMIT 1", nativeQuery = true)
     Optional<User> findByUsername(@Param("username") String username);
 
-    @EntityGraph(attributePaths = {"primaryRole", "roles"})
     @Query(value = "SELECT * FROM users WHERE email = :email LIMIT 1", nativeQuery = true)
     Optional<User> findByEmail(@Param("email") String email);
 
