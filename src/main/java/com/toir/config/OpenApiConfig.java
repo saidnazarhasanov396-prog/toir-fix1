4f288b8c7cf88b7fe66766a28486e67ffea5f925
kuanov007 <kuanovt1@gmail.com>
2026-04-25 17:46:21 +0500

    Swagger configuration fixed for default server

--- src/main/java/com/toir/config/OpenApiConfig.java
+++ src/main/java/com/toir/config/OpenApiConfig.java
@@ -9,6 +9,7 @@ import com.toir.security.AuthenticatedUser;
 import com.toir.security.JwtService;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.headers.Header;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
@@ -19,6 +20,7 @@ import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import java.util.*;
 
@@ -29,29 +31,44 @@ public class OpenApiConfig {
     private static final String SCHEME_NAME = "bearerAuth";
     private final UserRepository userRepository;
     private final JwtService jwtService;
-    @Value("${app.openapi.dev-url:http://localhost:8080}")
-    private String devUrl;
 
     @Value("${app.openapi.prod-url:https://api-toir.tenzorsoft.uz}")
     private String prodUrl;
 
+    @Value("${app.openapi.dev-url:http://localhost:8080}")
+    private String devUrl;
+
+    @Value("${spring.profiles.default}")
+    private String defaultProfile;
+
+
     @Bean
     public OpenAPI toirOpenAPI() {
-        Server devServer = new Server();
-        devServer.setUrl(devUrl);
-        devServer.setDescription("Locale");
+        List<Server> servers = new ArrayList<>();
 
         Server prodServer = new Server();
         prodServer.setUrl(prodUrl);
         prodServer.setDescription("Production");
+
+        Server devServer = new Server();
+        devServer.setUrl(devUrl);
+        devServer.setDescription("Local");
+
+        if (defaultProfile.contains("dev")) {
+            servers.add(devServer);
+            servers.add(prodServer);
+        } else {
+            servers.add(prodServer);
+            servers.add(devServer);
+        }
         String jwtToken = getAdminToken();
 
         return new OpenAPI()
                 .info(new Info()
                         .title("TOIR Backend API")
-                        .description("### Admin Token:\n `" + jwtToken + "`")
+                        .description("### Admin Token:\n```text\n" + jwtToken + "\n```")
                         .version("1.0.0"))
-                .servers(List.of(devServer, prodServer))
+                .servers(servers)
                 .addSecurityItem(new SecurityRequirement().addList(SCHEME_NAME))
                 .components(new Components().addSecuritySchemes(SCHEME_NAME,
                         new SecurityScheme()
@@ -99,6 +116,6 @@ public class OpenApiConfig {
 
             return jwtService.generateToken(principal.id(), principal.username(), List.copyOf(authorityCodes), extra);
         }
-        return "Token op kelolmadim";
+        return "TOKEN NOT FOUND!";
     }
 }
