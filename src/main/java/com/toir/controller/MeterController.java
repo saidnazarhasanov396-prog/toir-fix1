e7056f48169b2fe6ef37b21962841b44c2e645c2
Muhammadali <ekubzonovmuhammadali@email.com>
2026-04-28 16:57:09 +0500

    added equipment name to /api/v1/defects api

--- src/main/java/com/toir/controller/MeterController.java
+++ src/main/java/com/toir/controller/MeterController.java
@@ -1,6 +1,5 @@
 package com.toir.controller;
 import com.toir.dto.meter.MeterTriggerMatch;
-import com.toir.response.meter.EquipmentMeterResponse;
 import com.toir.service.MeterService;
 import com.toir.service.MeterTriggerService;
 
