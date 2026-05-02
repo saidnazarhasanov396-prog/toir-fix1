a7c92e1dc5ced512434687e75c66016546bec841
Ruziboyev Sarvar <sarvarruziboyev244@gmail.com>
2026-05-02 12:26:29 +0500

    DashboardService ga departmentId bo'yicha filter qo'shildi

--- src/main/java/com/toir/controller/DashboardController.java
+++ src/main/java/com/toir/controller/DashboardController.java
@@ -5,7 +5,10 @@ import com.toir.service.DashboardService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/dashboards")
@@ -19,7 +22,9 @@ public class DashboardController {
     }
 
     @GetMapping("/overview")
-    public DashboardOverview overview() {
-        return service.overview();
+    public DashboardOverview overview(
+            @RequestParam(required = false) UUID departmentId
+    ) {
+        return service.overview(departmentId);
     }
 }
