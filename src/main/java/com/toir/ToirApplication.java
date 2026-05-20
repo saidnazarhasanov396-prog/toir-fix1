828466f0cb6b407d484e5dc1c6792f592619bef6
Behzod <alzosidi@gmail.com>
2026-05-20 16:35:08 +0500

    prosta o'zgarish

--- src/main/java/com/toir/ToirApplication.java
+++ src/main/java/com/toir/ToirApplication.java
@@ -14,5 +14,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ToirApplication {
     public static void main(String[] args) {
         SpringApplication.run(ToirApplication.class, args);
+        System.out.println("Runned");
     }
 }
