597ae8b0fae2bebedd78cd74f0bacafaa9bab552
Behzod <alzosidi@gmail.com>
2026-05-26 09:33:21 +0500

    PPR plan migration tog'irlandi

--- src/main/java/com/toir/ToirApplication.java
+++ src/main/java/com/toir/ToirApplication.java
@@ -1,11 +1,9 @@
 package com.toir;
 
-import com.toir.security.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
@@ -14,6 +12,5 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ToirApplication {
     public static void main(String[] args) {
         SpringApplication.run(ToirApplication.class, args);
-        System.out.println("just rerun");
     }
 }
