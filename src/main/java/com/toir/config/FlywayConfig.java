5d76258604b21afc53b4fc2953af79ab80762de3
Doniyor <doniyork334@gmail.com>
2026-06-27 17:19:07 +0500

    repair config

--- src/main/java/com/toir/config/FlywayConfig.java
+++ src/main/java/com/toir/config/FlywayConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FlywayConfig {
 
+
+    /// Flyway checksum mismatch repairment configuration
     @Bean
     public FlywayMigrationStrategy cleanMigrateStrategy() {
         return flyway -> {
